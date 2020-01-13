ae4e5fa3a9d53a6ed6f5d96149419d1a65f69117
fikneader <fikneader@gmail.com>
2020-01-13 14:42:45 +0700

    Initial commit

---
+++ app/src/main/java/online/botika/firstkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package online.botika.firstkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
